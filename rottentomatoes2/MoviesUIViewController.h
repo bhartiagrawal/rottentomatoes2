5ef961449a419b17deacc1a0db73de993ee4ac94
Bharti Agrawal <bharti@yahoo-inc.com>
2014-06-10 22:46:33 -0700

    initial commit

---
+++ rottentomatoes2/MoviesUIViewController.h
@@ -0,0 +1,13 @@
+//
+//  MovieUIViewController.h
+//  rottentomatoes2
+//
+//  Created by Bharti Agrawal on 6/10/14.
+//  Copyright (c) 2014 Yahoo Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoviesUIViewController : UIViewController  <UITableViewDataSource>
+
+@end
---
+++ rottentomatoes2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  rottentomatoes2
+//
+//  Created by Bharti Agrawal on 6/10/14.
+//  Copyright (c) 2014 Yahoo Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
